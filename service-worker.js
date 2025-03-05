2c56ca8c4f832ae88f82a3a47063ba7e745f7bd9
tutukaX <shadowxtutukax@gmail.com>
2025-03-05 01:05:33 -0300

    Corregidas rutas de iconos

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-connect-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/assets/icon-192x192.png',
-  '/assets/icon-512x512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './assets/icon-192x192.png',
+  './assets/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
